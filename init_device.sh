99e80b63bffc926b5b772ee84a0ed1c8f4521e02
podxboq <podxboq@gmail.com>
2012-02-21 23:32:58 +0100

    New script for generate first files for build a device.

---
+++ init_device.sh
@@ -0,0 +1,98 @@
+#!/bin/sh
+
+# Copyright (C) 2012 The SuperTeam Developer Group
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+MANUFACTURER=$1
+DEVICE=$2
+
+mkdir -p device/$MANUFACTURER/$DEVICE
+
+(cat << EOF) | sed s/__DEVICE__/$DEVICE/g > device/$MANUFACTURER/$DEVICE/envsetup.sh
+# Copyright (C) 2012 The SuperTeam Developer Group
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+add_lunch_combo team___DEVICE__-eng
+
+EOF
+
+(cat << EOF) | sed s/__DEVICE__/$DEVICE/g > device/$MANUFACTURER/$DEVICE/AndroidBoard.mk
+# Copyright (C) 2012 The SuperTeam Developer Group
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+PRODUCT_MAKEFILES := \
+\$(LOCAL_DIR)/team___DEVICE__.mk
+
+EOF
+
+(cat << EOF) | sed s/__DEVICE__/$DEVICE/g | sed s/__MANUFACTURER__/$MANUFACTURER/g > device/$MANUFACTURER/$DEVICE/team_$DEVICE.mk
+# Copyright (C) 2012 The SuperTeam Developer Group
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+\$(call inherit-product, device/__MANUFACTURER__/__DEVICE__/__DEVICE__.mk)
+
+EOF
+
+(cat << EOF) | sed s/__DEVICE__/$DEVICE/g | sed s/__MANUFACTURER__/$MANUFACTURER/g > device/$MANUFACTURER/$DEVICE/$DEVICE.mk
+# Copyright (C) 2012 The SuperTeam Developer Group
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+\$(call inherit-product-if-exists, vendor/__MANUFACTURER__/__DEVICE__/__DEVICE__-vendor.mk)
+
+EOF
+

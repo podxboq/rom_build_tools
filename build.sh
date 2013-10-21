b361381ca280ca011818a340179c8aff89c2d2c7
podxboq <podxboq@gmail.com>
2013-10-21 23:21:26 +0200

    Clean code.

--- build.sh
+++ build.sh
@@ -14,43 +14,36 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
 #Inicializamos las variables
-SCRIPTDIR=`dirname $0`
+SCRIPTDIR=`readlink $0`
+if [ $SCRIPTDIR = "" ]
+then
+  SCRIPTDIR=`dirname $0`
+else
+  SCRIPTDIR=`dirname $0`/`dirname $SCRIPTDIR`
+fi
+
 TOPDIR=`pwd`
 . $SCRIPTDIR/mensajes.sh
 LOGFILE=error.log
 
 if [ $# -gt 3 ] || [ $# -lt 1 ]
 then
-	msgErr "Usage: $0 <device> [organization] [buildtype]"
-	exit 1
+  msgErr "Usage: $0 <device> [organization] [buildtype]"
+  exit 1
 fi
 
+DEVICE=$1
 ORG=$2
 if [ "$ORG" == "" ]
 then
-	ORG=osr
+  ORG=osr
 fi
 
 BUILD=$3
 if [ "$BUILD" == "" ]
 then
-	BUILD=eng
-fi
-
-MAINFILE=`find device -name $ORG\_$1.mk`
-if [ -z $MAINFILE ]
-then
-	msgErr "No se ha encontrado el dispositivo $1"
-	exit 1
-fi
-
-SUBDEVICE=`grep -G ^PRODUCT_SUBDEVICE $MAINFILE`
-if [ -n $SUBDEVICE ]; then
-	DEVICE=$1
-else
-	DEVICE=$SUBDEVICE
+  BUILD=eng
 fi
 
 ROMDIR=$TOPDIR/../cache/roms/$DEVICE
@@ -62,181 +55,179 @@ CONFIGFILE=$HOME/.SuperOSR.conf
 
 #Buscamos valores personalizados para el build
 if [ -f $CONFIGFILE ]; then
-	CORES=$( grep CORES $CONFIGFILE | cut -f 2 -d "=" )
-	if [ -z "$CORES" ]; then
-		CORES=$( cat /proc/cpuinfo | grep -c processor )
-	fi
-	USE_CCACHE=$( grep USE_CCACHE $CONFIGFILE | cut -f 2 -d "=" )
-	if [ -n "$USE_CCACHE" ] && [ "$USE_CCACHE" = "1" ]; then
-		export USE_CCACHE=1
-	else
-		unset USE_CCACHE
-	fi
+  CORES=$( grep CORES $CONFIGFILE | cut -f 2 -d "=" )
+  if [ -z "$CORES" ]; then
+    CORES=$( cat /proc/cpuinfo | grep -c processor )
+  fi
+  USE_CCACHE=$( grep USE_CCACHE $CONFIGFILE | cut -f 2 -d "=" )
+  if [ -n "$USE_CCACHE" ] && [ "$USE_CCACHE" = "1" ]; then
+    export USE_CCACHE=1
+  else
+    unset USE_CCACHE
+  fi
 fi
 
 function compilar(){
-	#borramos los objetos para forzar que se copie la ROM entera
-	rm -rf $OUT/recovery $OUT/root $OUT/system $OUT/kernel 2&> /dev/null
-	make -j${CORES} otapackage 2> $LOGFILE
-	if [ "$?" -eq 0 ]; then
-	    msgOK "Compilación correcta"
-	else
-	    msgErr "Error en compilación"
-	    FAIL=true
-	fi
-}	 
+  #borramos los objetos para forzar que se copie la ROM entera
+  rm -rf $OUT/recovery $OUT/root $OUT/system $OUT/kernel 2&> /dev/null
+  make -j${CORES} otapackage 2> $LOGFILE
+  if [ "$?" -eq 0 ]; then
+      msgOK "Compilación correcta"
+  else
+      msgErr "Error en compilación"
+      FAIL=true
+  fi
+}   
 
 function squishear(){
-	SQUISHER=`find vendor -name squisher.sh`
-	$SQUISHER
-	if [ "$?" -eq 0 ]; then
-	    msgOK "Personalización correcta"
-	else
-	    msgErr "Error al ejecutar squisher"
-	    FAIL=true
-	fi
+  $SCRIPTDIR/squisher.sh
+  if [ "$?" -eq 0 ]; then
+      msgOK "Personalización correcta"
+  else
+      msgErr "Error al ejecutar squisher"
+      FAIL=true
+  fi
 }
 
 function zipear(){
-	ZIPEAR=`find vendor -name zipear.sh`
-	$ZIPEAR
+  $SCRIPTDIR/zipear.sh
 }
 
 function buscar(){
-	echo "Texto: "
-	read text
-	echo "1: Módulo"
-	echo "2: Make"
-	echo "3: C/C++"
-	echo "4: Java"
-	read option
-	if [ ! -z $option ]
-	then
-		case $option in
-			1) mgrep LOCAL_MODULE | grep $text\$;;
-			2) mgrep "$text";;
-			3) cgrep "$text";;
-			4) jgrep "$text";;
-		esac
-	fi
+  echo "Texto: "
+  read text
+  echo "1: Módulo"
+  echo "2: Make"
+  echo "3: C/C++"
+  echo "4: Java"
+  read option
+  if [ ! -z $option ]
+  then
+    case $option in
+      1) mgrep LOCAL_MODULE | grep $text\$;;
+      2) mgrep "$text";;
+      3) cgrep "$text";;
+      4) jgrep "$text";;
+    esac
+  fi
 }
-	
+  
 function reiniciar(){
-	echo "1: Normal"
-	echo "2: Recovery"
-	echo "3: HBoot"
-	echo "4: Apagar"
-	read option
-	if [ ! -z $option ]
-	then
-		case $option in
-			1) adb reboot;;
-			2) adb reboot recovery;;
-			3) adb reboot bootloader;;
-			4) adb shell halt;;
-		esac
-	fi
+  echo "1: Normal"
+  echo "2: Recovery"
+  echo "3: HBoot"
+  echo "4: Apagar"
+  read option
+  if [ ! -z $option ]
+  then
+    case $option in
+      1) adb reboot;;
+      2) adb reboot recovery;;
+      3) adb reboot bootloader;;
+      4) adb shell halt;;
+    esac
+  fi
 }
-	
+  
 function sincronizar(){
-   	$SCRIPTDIR/sincronizar.sh $ROMDIR
-	if [ "$?" -eq 0 ]; then
-	    msgOK "Sincronización correcta"
-	else
-	    msgErr "Error al sincronizar"
-	    FAIL=true
-	fi
+     $SCRIPTDIR/sincronizar.sh $ROMDIR
+  if [ "$?" -eq 0 ]; then
+      msgOK "Sincronización correcta"
+  else
+      msgErr "Error al sincronizar"
+      FAIL=true
+  fi
 }
 
 function makeClean(){
-	echo "¿Estás seguro que quieres empezar de cero? (s/N)"
-	read option
-	option=${option:="N"}
-	if [ "$option" = "s" ] || [ "$option" = "S" ]; then
-		make clean
-	fi
+  echo "¿Estás seguro que quieres empezar de cero? (s/N)"
+  read option
+  option=${option:="N"}
+  if [ "$option" = "s" ] || [ "$option" = "S" ]; then
+    make clean
+  fi
 }
 
 function parchear(){
-	if [ ! -d $PUBLICDIR ]; then
-		msgWarn "No existe un directorio con la versión actualmente publicada. Se crea uno nuevo. La propia ROM es el parche."
-		cp -r $BUILDDIR $PUBLICDIR
-	else
-		if [ -d $PATCHDIR ]; then
-			rm -r $PATCHDIR
-		fi
-		mkdir $PATCHDIR
-		msgStatus "Calculando las diferencias con la anterior versión publicada"
-		$SCRIPTDIR/sacadiff.sh $BUILDDIR $PUBLICDIR $ROMDIR/public.diff.txt
-		$SCRIPTDIR/fromdiff.sh $ROMDIR/public.diff.txt $PATCHDIR patch
-		$SCRIPTDIR/updater.sh $DEVICE
-	fi
+  if [ ! -d $PUBLICDIR ]; then
+    msgWarn "No existe un directorio con la versión actualmente publicada. Se crea uno nuevo. La propia ROM es el parche."
+    cp -r $BUILDDIR $PUBLICDIR
+  else
+    if [ -d $PATCHDIR ]; then
+      rm -r $PATCHDIR
+    fi
+    mkdir $PATCHDIR
+    msgStatus "Calculando las diferencias con la anterior versión publicada"
+    $SCRIPTDIR/sacadiff.sh $BUILDDIR $PUBLICDIR $ROMDIR/public.diff.txt
+    $SCRIPTDIR/fromdiff.sh $ROMDIR/public.diff.txt $PATCHDIR patch
+    $SCRIPTDIR/updater.sh $DEVICE
+  fi
 }
 
 while true
 do
-    #inicializamos estados
-	msgStatus "Compilando la rom del SuperTeam para el dispositivo $1"
-    echo "Elige una opción:"
-    echo " 1: make"
-    echo " 2: squisher"
-    echo " 3: zip"
-    echo " 4: sincronizar"
-    echo " 5: crear parche"
-    echo " 6: make + squisher + zip + sincronizar"
-    echo " 7: limpiar build"
-    echo " 8: Reiniciar/Apagar dispositivo"
-    echo " 9: Compilar kernel"
-    echo "10: Cambiar boot"
-    echo "11: Copiar ROM al dispositivo"
-    echo "12: Buscar"
-    echo "13: Ver fichero de errores"
-    echo "99: salir"
+  #inicializamos estados
+  msgStatus "Compilando la rom del SuperTeam para el dispositivo $1"
+  echo "Elige una opción:"
+  echo " 1: make"
+  echo " 2: squisher"
+  echo " 3: zip"
+  echo " 4: sincronizar"
+  echo " 5: crear parche"
+  echo " 6: make + squisher + zip + sincronizar"
+  echo " 7: limpiar build"
+  echo " 8: Reiniciar/Apagar dispositivo"
+  echo " 9: Compilar kernel"
+  echo "10: Cambiar boot"
+  echo "11: Copiar ROM al dispositivo"
+  echo "12: Buscar"
+  echo "13: Ver fichero de errores"
+  echo "99: salir"
 
-    read option
+  read option
     
-    FAIL=false
+  FAIL=false
     
-    if [ $option -eq 99 ]; then
-        exit 0
-    fi
+  if [ $option -eq 99 ]; then
+    exit 0
+  fi
 
-    if [ "$OUT" = "" ]; then
-    	. build/envsetup.sh
-    	lunch "$ORG"_"$DEVICE"-"$BUILD"
-        if [ "$?" -ne 0 ]; then
-            continue
-        fi
+  if [ "$OUT" = "" ]; then
+    . build/envsetup.sh
+    lunch "$ORG"_"$DEVICE"-"$BUILD"
+    if [ "$?" -ne 0 ]; then
+      continue
     fi
+  fi
     
-    case $option in
-    	1) compilar;;
-	2) squishear;;
-	3) zipear;;
-	4) sincronizar;;
-	5) parchear;;
-	6) compilar
-		if ! $FAIL ; then
-			squishear
-		fi
-		if ! $FAIL ; then
-			sincronizar
-		fi
-		if ! $FAIL ; then
-			zipear
-		fi;;
-    	7) makeClean;;
-    	8) reiniciar;;
-    	9) $SCRIPTDIR/kernel.sh $DEVICE;;
-    	10) fastboot flash boot $OUT/boot.img;;
-    	11) echo "Copiando $OUT/$PRODUCT_ROM_FILE.zip"; adb remount; adb push $OUT/$PRODUCT_ROM_FILE.zip /mnt/sdcard/
-    		if [ "$?" -eq 0 ]; then
-			msgOK "OK"
-		else
-			msgErr "Error al copiar la ROM"
-		fi;;
-	12) buscar;;
-	13) cat $LOGFILE;;	
-    esac    
+  case $option in
+    1) compilar;;
+    2) squishear;;
+    3) zipear;;
+    4) sincronizar;;
+    5) parchear;;
+    6) compilar
+       if ! $FAIL ; then
+         squishear
+       fi
+       if ! $FAIL ; then
+         sincronizar
+       fi
+       if ! $FAIL ; then
+         zipear
+       fi;;
+    7) makeClean;;
+    8) reiniciar;;
+    9) $SCRIPTDIR/kernel.sh $DEVICE;;
+    10) fastboot flash boot $OUT/boot.img;;
+    11) echo "Copiando $OUT/$PRODUCT_ROM_FILE.zip"; adb remount; adb push $OUT/$PRODUCT_ROM_FILE.zip /mnt/sdcard/
+        if [ "$?" -eq 0 ]; then
+          msgOK "OK"
+        else
+          msgErr "Error al copiar la ROM"
+        fi;;
+    12) buscar;;
+    13) cat $LOGFILE;;
+  esac    
 done
-	
+  
